a25b27317d85f886f0f9ae46776ad3c92bd57253
RichardJhn <rjohn@my.yearupunited.org>
2025-11-07 09:09:51 -0500

    made sandwich a child class and made its super

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -1,4 +1,66 @@
 package com.pluralsight;
 
-public class Sandwich {
+public class Sandwich extends Items {
+    //this will be a child class of Items as it will contain its name and price
+
+    private String size;
+    private String bread;
+    private boolean toasted;
+    private String toppings; //to do - find a way to have my list of toppings
+
+    public Sandwich(String itemName, double itemPrice, String size, String bread, boolean toasted, String toppings) {
+        super(itemName, itemPrice);
+        this.size = size;
+        this.bread = bread;
+        this.toasted = toasted;
+        this.toppings = toppings;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
+
+    public boolean isToasted() {
+        return toasted;
+    }
+
+    public void setToasted(boolean toasted) {
+        this.toasted = toasted;
+    }
+
+    public String getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String toppings) {
+        this.toppings = toppings;
+    }
+
+    public double price(){
+
+        //returning 0 until i do my calculations
+        return 0;
+    }
+
+    @Override
+    public String toString() {  //format will be a place holder until i finish everything else
+        return "Sandwich{" +
+                "size='" + size + '\'' +
+                ", bread='" + bread + '\'' +
+                ", toasted=" + toasted +
+                ", toppings='" + toppings + '\'' +
+                '}';
+    }
 }
