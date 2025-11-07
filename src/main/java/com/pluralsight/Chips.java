895ff845c0cb4b5815ff787ea09f8462a8de7ef0
RichardJhn <rjohn@my.yearupunited.org>
2025-11-07 12:54:17 -0500

    mostly finished drink class

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -7,6 +7,8 @@ public class Chips extends Items {
     //chipType will be the brand or flavor they choose
      private String chipType;
      Scanner scanner = new Scanner(System.in);
+     String userInput = scanner.nextLine().trim();
+
 
     public Chips(String itemName, double itemPrice, String chipType) {
         super(itemName, itemPrice);
@@ -18,6 +20,8 @@ public class Chips extends Items {
     }
     public double price(){
         //this will return 0 until i make the calculations
+        System.out.print("What kind of chips would you like: ");
+        chipType = userInput;
         itemPrice = 1.50;
        return itemPrice;
     }
--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,11 +1,13 @@
 package com.pluralsight;
-
+import java.util.Scanner;
 public class Drink extends Items{
     //will have my drink options
     //to do - determine how I will be adding flavors
 
     private String flavor;
     private String size;
+    Scanner scanner = new Scanner(System.in);
+    String userInput = scanner.nextLine().trim();
 
     public Drink(String itemName, double itemPrice, String flavor, String size) {
         super(itemName, itemPrice);
@@ -23,8 +25,23 @@ public class Drink extends Items{
 
     public double price(){
         //return 0 until i make the calculations
-        return 0;
+        System.out.print("What kind of Drink would you like: ");
+        flavor = userInput;
+        System.out.println("What size would you like:");
+        switch (userInput){
+            case "1":
+                itemPrice = 2.00;
+            case "2":
+                itemPrice = 2.50;
+            case "3":
+                itemPrice = 3.00;
+            default:
+                System.out.println("Error!");
+        }
+        
+        return itemPrice;
     }
+
 //adding to string just in case its needed
     @Override
     public String toString() {
