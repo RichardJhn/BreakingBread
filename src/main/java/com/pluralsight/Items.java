bf542e50d0016b4b16fdb54f051f37194b7e874a
RichardJhn <rjohn@my.yearupunited.org>
2025-11-06 22:43:02 -0500

    finished Orderscreen layout

--- src/main/java/com/pluralsight/Items.java
+++ src/main/java/com/pluralsight/Items.java
@@ -1,4 +1,7 @@
 package com.pluralsight;
 
 public class Items {
+
+    //to do - make this an parent class
+    //make all menu items a child of this abbstract class
 }
--- src/main/java/com/pluralsight/OrderScreen.java
+++ src/main/java/com/pluralsight/OrderScreen.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
-
+import java.util.Scanner;
 public class OrderScreen {
+    Scanner scanner = new Scanner(System.in);
+    public void loadOrderScreen(){
+
+        String choice = "";
+        while (!choice.equalsIgnoreCase("0")){
+            System.out.println("""
+                    ========Order Menu ========
+                    1) Add Sandwich
+                    2) Add Drink
+                    3) Add Chips
+                    4) Checkout
+                    0) Cancel Order
+                    ============================
+                    Please Make your choice
+                    ============================
+                    """);
+
+            choice = scanner.nextLine().trim();
+
+            switch(choice.toString()){
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "0":
+                default:
+                    System.out.println("Error!");
+            }
+
+        }
+
+    }
 }
