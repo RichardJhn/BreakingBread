7da24f60100dba2ece8543c723593f30c099013b
RichardJhn <rjohn@my.yearupunited.org>
2025-11-07 09:14:47 -0500

    made chips a child class of items

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
-public class Chips {
+public class Chips extends Items {
+    //chipType will be the brand or flavor they choose
+     private String chipType;
+
+    public Chips(String itemName, double itemPrice, String chipType) {
+        super(itemName, itemPrice);
+        this.chipType = chipType;
+    }
+
+    public String getChipType() {
+        return chipType;
+    }
+    public double price(){
+        //this will return 0 until i make the calculations
+        return 0;
+    }
+//not sure if i need a to string here but will add just in case
+    @Override
+    public String toString() {
+        return "Chips{" +
+                "chipType='" + chipType + '\'' +
+                '}';
+    }
 }
