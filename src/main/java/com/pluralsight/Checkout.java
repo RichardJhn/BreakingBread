e03f412f7b6c31b1c669e70f83a8b1a5ed8d012b
RichardJhn <rjohn@my.yearupunited.org>
2025-11-06 23:21:22 -0500

    added checkout screen and should work

--- src/main/java/com/pluralsight/Checkout.java
+++ src/main/java/com/pluralsight/Checkout.java
@@ -1,7 +1,35 @@
 package com.pluralsight;
-
+import java.util.Scanner;
 public class Checkout {
+    Scanner scanner = new Scanner(System.in);
     public void displayCheckout(){
+
+        //make a simple checkout menu for now
+        //to do - code the deletion of all options and add a method to save to new file for every new customer
+
+        String choice = "";
+        while (!choice.equalsIgnoreCase("0")){
+            System.out.println("""
+                    ==========Checkout=========
+                    1) Confirm
+                    0) Cancel
+                    """);
+
+            choice = scanner.nextLine();
+
+            switch (choice.toString()){
+                case "1":
+                    //placeholder for Confirm
+                    break;
+                case "2":
+                    //placeholder for cancel
+                    break;
+                default:
+                    System.out.println("Error!");
+            }
+
+        }
+
 
     }
 }
