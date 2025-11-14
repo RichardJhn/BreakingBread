958f82d4b7935dd202d56f01705bb7b04410f759
RichardJhn <rjohn@my.yearupunited.org>
2025-11-13 19:20:48 -0500

    started readme working on signature

--- src/main/java/com/pluralsight/Sellables/Specialties.java
+++ src/main/java/com/pluralsight/Sellables/Specialties.java
@@ -1,5 +1,23 @@
 package com.pluralsight.Sellables;
 
-public class Specialties {
+public class Specialties extends Sandwich{
     // I WILL GET THIS DONE!!!!
+
+    public Sandwich theBreakingOfBreads() {
+        Sandwich sandwich = new Sandwich();
+        sandwich.setSize("Large");
+        sandwich.setBread("Wheat");
+        sandwich.setToasted(true);
+        
+        Topping topping = new Topping("Steak","Meat",10);
+        Topping topping2 = new Topping("American","Cheese",12);
+
+        return sandwich;
+    }
+
+
+
+
+
+
 }
